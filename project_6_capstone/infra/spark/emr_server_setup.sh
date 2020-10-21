f3011721a8522fd3c5108c7c23be186d11141260
Marawan Okasha <marawanokasha@yahoo.com>
2020-10-21 23:23:20 +0200

    added keyspaces setup to the emr server setup script

--- project_6_capstone/infra/spark/emr_server_setup.sh
+++ project_6_capstone/infra/spark/emr_server_setup.sh
@@ -22,3 +22,20 @@ jupyter nbextension enable --user --py widgetsnbextension
 # sudo su
 # jupyter nbextension enable --system --py widgetsnbextension
 # jupyter contrib nbextension install --system
+
+
+
+###############################################
+# Amazon Keyspaces
+#
+# Downloading and adding the amazon certificate to a keystore so it could be used when connecting to keyspaces
+# Check https://docs.aws.amazon.com/keyspaces/latest/devguide/using_java_driver.html for more
+
+curl https://www.amazontrust.com/repository/AmazonRootCA1.pem -O
+
+KEYSPACES_KEYSTORE_PASS=rUvKYVb_7CF734J_
+
+openssl x509 -outform der -in AmazonRootCA1.pem -out amazon_cert_file.der
+keytool -import -alias cassandra -noprompt -storepass $KEYSPACES_KEYSTORE_PASS -keystore cassandra_truststore.jks -file amazon_cert_file.der
+
+cp ./cassandra_truststore.jks /home/hadoop/cassandra_truststore.jks
